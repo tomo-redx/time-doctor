ce4dd2ef538d8806ec081acb95bdec196efcbee0
Tomohiro Kinoshita <tomorrowredx@gmail.com>
2024-06-09 12:25:44 +0900

    first commit

---
+++ src/popup/Popup.tsx
@@ -0,0 +1,13 @@
+import { Counter } from '../app/features/counter';
+
+const Popup = () => {
+  document.body.className = 'w-[30rem] h-[15rem]';
+
+  return (
+    <div>
+      <button>Dashboard</button>
+    </div>
+  );
+};
+
+export default Popup;
